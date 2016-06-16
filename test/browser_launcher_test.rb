82a629ea5bef5aab8dde05900b128cc92763e80e
Anup Cowkur <anupcowkur89@gmail.com>
2016-06-16 18:42:58 +0530

    Use mocha in browser launcher tests

--- test/browser_launcher_test.rb
+++ test/browser_launcher_test.rb
@@ -3,11 +3,7 @@ require 'browser_launcher'
 
 class BrowserLauncherTest < Minitest::Test
   def test_opens_browser
-    mock = MiniTest::Mock.new
-    mock.expect(:call, nil, ["link"])
-    Launchy.stub(:open, mock) do
-      BrowserLauncher.new.launch("link")
-    end
-    mock.verify
+  	Launchy.expects(:open).with("link")
+  	BrowserLauncher.new.launch("link")
   end
 end
