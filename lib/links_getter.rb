dffd34aeb74227eb78e8b4cc0a91a79c50960acf
Anup Cowkur <anupcowkur89@gmail.com>
2016-06-17 10:54:55 +0530

    Shorten file reading in links getter and it's test

--- lib/links_getter.rb
+++ lib/links_getter.rb
@@ -1,8 +1,6 @@
 class LinksGetter
   def get_links_from_file
-    file = File.open(File.expand_path("../assets/lists.js", File.dirname(__FILE__)), "r")
-    contents = file.read
-    file.close
+    contents = File.read(File.expand_path("../assets/lists.js", File.dirname(__FILE__)))
 
     return contents.partition("[").last.partition("]").first.split("\n")
   end
--- test/links_getter_test.rb
+++ test/links_getter_test.rb
@@ -4,10 +4,8 @@ require 'links_getter'
 class TestLinksHelper < Minitest::Test
 
   def test_that_it_gets_links_from_file
-    file = File.open(File.expand_path('../test/fixtures/test_links_array.js', File.dirname(__FILE__)), "r")
-    expected_links = file.read
-    file.close
-
+    expected_links = File.read(File.expand_path('../test/fixtures/test_links_array.js', File.dirname(__FILE__)))
+   
     result_links = LinksGetter.new.get_links_from_file.to_s
 
     assert_equal expected_links, result_links
